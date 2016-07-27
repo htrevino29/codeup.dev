fe8531ea62cc6ebe2221414796530366e615163b
htrevino29 <htrevino29@gmail.com>
2016-07-27 17:11:25 -0500

    Initial commit

---
+++ public/js/if-else.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// Question 1
+var average = (70 + 80 + 95) / 3
+// console.log(average)
+if (average >= 79) {
+	
+    // code here gets executed if condition evaluates to true
+    
+    console.log("you're awesome")
+
+} else {
+    // code here gets executed if condition evaluates to false
+    console.log("you need more practice more")
+}
+
+// Question 2
+
+
+
+///
+var person = "Cam";
+var salesAmount = 180;
+var discount = 0.35;
+var finalAmount;
+var message;
+
+if(salesAmount > 200) {
+	finalAmount = salesAmount - (salesAmount * discount);
+	console.log(person + " purchased $" + salesAmount + " and a discount was applied. Final amount is: $" + finalAmount
+} else {
+	finalAmount = salesAmount;
+	console.log(person + " purchased $") + salesAmount + " and a discount was NOT applied. Final amount is: $" + finalAmount
+}
+
+person = "Ryan";
+salesAmount = 250;
+
+if(salesAmount > 200) {
+	finalAmount = salesAmount - (salesAmount * discount);
+	console.log(person + " purchased $" + salesAmount + " and a discount was applied. Final amount is: $" + finalAmount
+} else {
+	finalAmount = salesAmount;
+	console.log(person + " purchased $") + salesAmount + " and a discount was NOT applied. Final amount is: $" + finalAmount
+}
+
+person = "George";
+salesAmount = 320;
+
+if(salesAmount > 200) {
+	finalAmount = salesAmount - (salesAmount * discount);
+	console.log(person + " purchased $" + salesAmount + " and a discount was applied. Final amount is: $" + finalAmount
+} else {
+	finalAmount = salesAmount;
+	console.log(person + " purchased $") + salesAmount + " and a discount was NOT applied. Final amount is: $" + finalAmount
+}
+
+///
+
+//question 3
+
+var flipACoin = Math.floor(Math.random()* 2
+
+if 0 {
+	console.log("Issac buys a car")
+} else {
+	console.log("issac buys a house")
+}
+
+ 
+
+
+
