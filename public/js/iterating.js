6f681ca4a5979199946eaaeb28f60ff975cc0aa9
htrevino29 <htrevino29@gmail.com>
2016-08-02 14:20:02 -0500

    initial commit

---
+++ public/js/iterating.js
@@ -0,0 +1,12 @@
+(function(){
+    "use strict";
+
+    var peoplesNames = ["Zach", "Ryan", "Will", "Luis"]
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+    	console.log(peoplesNames.length);
+    // TODO: Create a log statement that will log the number of elements in the names array.
+    	for (var i = 0; i < peoplesNames.length; i +=1){
+			console.log(peoplesNames[i]);
+		}
+    // TODO: Create log statements that will print each of the names array elements individually.
+})();
