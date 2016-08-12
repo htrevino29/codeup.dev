2991a1584bfdc1a5803be6fc4848b18c232d475c
htrevino29 <htrevino29@gmail.com>
2016-08-12 15:01:45 -0500

    national parks update

--- public/js/jquery_faq_js.js
+++ public/js/jquery_faq_js.js
@@ -1,21 +1,24 @@
 "use strict";
 // (function(){
 
-// $("dt").toggle();
-
-
-// $( "h1" ).click(function() {
-// 	$('dd').toggle();
-// 	$('dt').toggle();
-    
-//   });
-
-// });
-
 $(document).ready(function () {
 	$('dd').addClass('invisible');
 
 	$('#toggle-answers').click(function() {
 		$('dd').toggleClass('invisible');
 	});
+});
+
+
+//Create jQuery code that makes the first li element in each ul have font-weight: bold.
+
+$('ul').each(function(index) {
+    //will loop through each group of UL
+    $(this).children().first().css('font-weight', 'bold');
+    //"this" grabs a specific UL 
+});
+
+$('li').click(function() {
+    $(this).parent().addClass('invisible');
+    
 });
