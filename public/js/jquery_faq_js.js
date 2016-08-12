7f9613a6c50087c4add9929378589a8bb96c3c57
htrevino29 <htrevino29@gmail.com>
2016-08-12 16:49:42 -0500

    toggle command

--- public/js/jquery_faq_js.js
+++ public/js/jquery_faq_js.js
@@ -1,24 +1,43 @@
 "use strict";
 // (function(){
 
-$(document).ready(function () {
-	$('dd').addClass('invisible');
+// $(document).ready(function () {
+// 	$('dd').addClass('invisible');
 
-	$('#toggle-answers').click(function() {
-		$('dd').toggleClass('invisible');
-	});
-});
+// 	$('#toggle-answers').click(function() {
+// 		$('dd').toggleClass('invisible');
+// 	});
+// });
 
 
 //Create jQuery code that makes the first li element in each ul have font-weight: bold.
 
-$('ul').each(function(index) {
-    //will loop through each group of UL
-    $(this).children().first().css('font-weight', 'bold');
-    //"this" grabs a specific UL 
+// $('ul').each(function(index) {
+//     //will loop through each group of UL
+//     $(this).children().first().css('font-weight', 'bold');
+//     //"this" grabs a specific UL 
+// });
+
+// $('li').click(function() {
+//     $(this).parent().addClass('invisible');
+    
+// });
+
+//Update the code that uses the invisible class to 
+//hide and show dd elements to instead use the .toggle() method.
+
+
+$(document).ready(function () {
+	$('dd').addClass('invisible');
+
+	$('#toggle-answers').click(function() {
+		$('dd').fadeToggle();
+	});
 });
 
-$('li').click(function() {
-    $(this).parent().addClass('invisible');
+
+$('h3').click(function() {
+    $(this).parent().children('ul').children('li').hide();
     
 });
+
