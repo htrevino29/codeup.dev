69c9552208b866b01594180fc13694f6e481c033
htrevino29 <htrevino29@gmail.com>
2016-08-16 15:59:16 -0500

    edit to map api

--- public/js/weather-map.js
+++ public/js/weather-map.js
@@ -30,30 +30,30 @@ function addWeather (forcast) {
 
 	//////////city content///////////
 	var day1 = '';
-		day1 += '<p>' + weatherObject.list[0].temp.max + "°" + ' / ' + weatherObject.list[0].temp.min + "°" + '</p>';
+		day1 += '<p><strong>' + weatherObject.list[0].temp.max + "°" + ' / ' + weatherObject.list[0].temp.min + "°" + '</strong></p>';
 		day1 += '<img src=http://openweathermap.org/img/w/' + weatherObject.list[0].weather[0].icon +'.png>' ;		
-		day1 += '<p>' + weatherObject.list[0].weather[0].description + '</p>';	
-		day1 += '<p>' + 'humidity: ' + weatherObject.list[0].humidity + '</p>';	
-		day1 += '<p>' + 'wind: ' + weatherObject.list[0].speed + '</p>';
-		day1 += '<p>' + 'pressure: ' + weatherObject.list[0].pressure + '</p>';
+		day1 += '<p><strong>' + weatherObject.list[0].weather[0].description + '</strong></p>';	
+		day1 += '<p>' + '<strong>humidity:</strong> ' + weatherObject.list[0].humidity + '</p>';	
+		day1 += '<p>' + '<strong>wind:</strong> ' + weatherObject.list[0].speed + '</p>';
+		day1 += '<p>' + '<strong>pressure:</strong> ' + weatherObject.list[0].pressure + '</p>';
 	$('#todaysWeather').append(day1);
 
 	var day2 = '';
-		day2 += '<p>' + weatherObject.list[1].temp.max + "°" + ' / ' + weatherObject.list[0].temp.min + "°" + '</p>';
+		day2 += '<p><strong>' + weatherObject.list[1].temp.max + "°" + ' / ' + weatherObject.list[0].temp.min + "°" + '</strong></p>';
 		day2 += '<img src=http://openweathermap.org/img/w/' + weatherObject.list[1].weather[0].icon +'.png>' ;
-		day2 += '<p>' + weatherObject.list[1].weather[0].description + '</p>';	
-		day2 += '<p>' + 'humidity: ' + weatherObject.list[1].humidity + '</p>';	
-		day2 += '<p>' + 'wind: ' + weatherObject.list[1].speed + '</p>';
-		day2 += '<p>' + 'pressure: ' + weatherObject.list[1].pressure + '</p>';
+		day2 += '<p><strong>' + weatherObject.list[1].weather[0].description + '</strong></p>';	
+		day2 += '<p>' + '<strong>humidity:</strong> ' + weatherObject.list[1].humidity + '</p>';	
+		day2 += '<p>' + '<strong>wind:</strong> ' + weatherObject.list[1].speed + '</p>';
+		day2 += '<p>' + '<strong>pressure:</strong> ' + weatherObject.list[1].pressure + '</p>';
 	$('#tomorrow').append(day2);
 
 	var day3 = '';
-		day3 += '<p>' + weatherObject.list[2].temp.max + "°" + ' / ' + weatherObject.list[0].temp.min + "°" + '</p>';
+		day3 += '<p><strong>' + weatherObject.list[2].temp.max + "°" + ' / ' + weatherObject.list[0].temp.min + "°" + '</strong></p>';
 		day3 += '<img src=http://openweathermap.org/img/w/' + weatherObject.list[2].weather[0].icon +'.png>' ;
-		day3 += '<p>' + weatherObject.list[2].weather[0].description + '</p>';	
-		day3 += '<p>' + 'humidity: ' + weatherObject.list[2].humidity + '</p>';	
-		day3 += '<p>' + 'wind: ' + weatherObject.list[2].speed + '</p>';
-		day3 += '<p>' + 'pressure: ' + weatherObject.list[2].pressure + '</p>';
+		day3 += '<p><strong>' + weatherObject.list[2].weather[0].description + '</strong></p>';	
+		day3 += '<p>' + '<strong>humidity:</strong> ' + weatherObject.list[2].humidity + '</p>';	
+		day3 += '<p>' + '<strong>wind:</strong> ' + weatherObject.list[2].speed + '</p>';
+		day3 += '<p>' + '<strong>pressure:</strong> ' + weatherObject.list[2].pressure + '</p>';
 	$('#dayAfter').append(day3);
 }
 
