e54bb89b7711703a4607d78db058329aaa21bff9
htrevino29 <htrevino29@gmail.com>
2016-09-09 15:30:50 -0500

    form preventing trolls from trolling.
    login page now has die(); enabled.

--- public/login.php
+++ public/login.php
@@ -1,23 +1,30 @@
 <?php
 
-$redirectTo = '/authorized.php';
-$username = isset($_POST['username']) ? $_POST['username'] : '';
-$password = isset($_POST['password']) ? $_POST['password'] : '';
-if($username == 'guest' && $password == 'password'){
-	echo $username;
-	echo $password;
-	header("Location: $redirectTo");
-} if($username == '' && $password == '')  {
-	echo $username;
-	echo $password;
+function pageController()
+{
+	$redirectTo = '/authorized.php';
+	$username = isset($_POST['username']) ? $_POST['username'] : '';
+	$password = isset($_POST['password']) ? $_POST['password'] : '';
+	if($username == 'guest' && $password == 'password'){
+		header("Location: $redirectTo");
+		die();
+	} if($username == '' && $password == '')  {
+		echo $username;
+		echo $password;
+	}
+	else {
+		echo "<script>
+		alert('username or password invalid');
+		</script>";	
+	}
+	return [
+		'username' => $username,
+		'password' => $password,
 
+	];
 }
-else {
-	echo "<script>
-	alert('username or password invalid');
-	</script>";
-	
-}
+
+extract(pageController());
 
 ?>
 <!DOCTYPE html>
@@ -28,13 +35,13 @@ else {
 </head>
 <body>
 	<div class='container'>
-		<h1>Sign in now!</h1>
+		<h1>Sign in </h1>
 	    <form method="POST">
-	        <label>Username:</label>
+	        <label>Username:</label><br>
 	        <input type="text" name="username"><br>
-	        <label>Password:</label>
+	        <label>Password:</label><br>
 	        <input type="password" name="password"><br>
-	        <input type="submit" class='btn'>
+	        <input type="submit" class=' btn btn-default'>
 	    </form>
 	</div>
     <script src="/js/bootstrap.min.js"></script>
