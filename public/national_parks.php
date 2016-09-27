2705498f46fcce396646433e68198255fefefadb
htrevino29 <htrevino29@gmail.com>
2016-09-27 12:15:15 -0500

    added prepare and park input.

--- public/national_parks.php
+++ public/national_parks.php
@@ -3,23 +3,40 @@
     require '../config.php';
     require_once '../db_connect.php';
 
-    function pageController($dbc){        
+    function pageController($dbc){  
+        if(!empty($_POST)){
+            $query= "INSERT INTO national_parks(name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)";
+            $stmt = $dbc->prepare($query);
+            $stmt->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+            $stmt->bindValue(':location',  $_POST['location'],  PDO::PARAM_STR);
+            $stmt->bindValue(':date_established', $_POST['date_established'], PDO::PARAM_STR);
+            $stmt->bindValue(':area_in_acres',  $_POST['area_in_acres'],  PDO::PARAM_STR);
+            $stmt->bindValue(':description', $_POST['description'], PDO::PARAM_STR);
+
+            $stmt->execute();
+        }      
     //    ternary statement reads GET request                
         $offset = (!empty($_GET)) ? $_GET["offset"] : 0;        // GET request NOT empty? if true, set it to offset => value. If false, set it to 0
     //    $stmt variable created to contain instance of PDOStatement representing the results of query
-        $stmt = $dbc->query("SELECT * FROM national_parks");    // query() method iterates through national_parks and retrieves metadata
-
+        $query= ("SELECT * FROM national_parks");
+        $stmt = $dbc->prepare($query);   // query() method iterates through national_parks and retrieves metadata
+        $stmt->execute();
+        $query2=('SELECT * FROM national_parks LIMIT 4 OFFSET ' . $offset);
+        $stmt2 = $dbc->prepare($query2);
+        $stmt2->execute();
+        $parks = $stmt2->fetchAll(PDO::FETCH_ASSOC);
     //    return statement returns parks key parkCount key
         return [
         //    retrieves info from national_parks and limits to 4 per page 
-            'parks' => $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET ' . $offset)->fetchAll(PDO::FETCH_ASSOC),    // concats the offset amount
+            'parks' => $parks,  // concats the offset amount
         //    parkCount key will count total number of parks in table
             'parkCount' => $stmt->rowCount()
         ];
     };
 
     extract(pageController($dbc));    // extracts pageController and passes $dbc from db_connect.php into it
-
+    
+    
 ?>
 
 <!DOCTYPE html>
@@ -58,6 +75,41 @@
         <!-- offset key is created here. anchor tag placed within for loop so as to create one for every 4 parks -->
             <a href="national_parks.php?offset=<?=$i?>"> <div class="btn btn-primary"> <?=$page++?></div></a>     
         <?php endfor; ?>
+        <br>
+        <h1>ADD PARK</h1>
+        <form method="post">
+            <div class="form-group row">
+              <label class="col-xs-2 col-form-label">Name</label>
+              <div class="col-xs-10">
+                <input class="form-control"  id="example-text-input" name='name'>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label  class="col-xs-2 col-form-label" >Location</label>
+              <div class="col-xs-10">
+                <input class="form-control"   id="example-search-input" name='location'>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-2 col-form-label">Date Established<br>(YYYY-MM-DD) </label>
+              <div class="col-xs-10">
+                <input class="form-control"   id="example-email-input" name='date_established'>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-2 col-form-label">Area in acres</label>
+              <div class="col-xs-10">
+                <input class="form-control"   id="example-url-input" name='area_in_acres'>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-2 col-form-label">Description</label>
+              <div class="col-xs-10">
+                <input class="form-control"  id="example-tel-input" name='description'>
+              </div>
+            </div>
+             <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
 </body>
 </html>
