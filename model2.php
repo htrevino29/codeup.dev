e32dc45c86f81a66aee04d9e243f906b42a5fb06
htrevino29 <htrevino29@gmail.com>
2016-10-03 09:51:05 -0500

    create a model exercise

---
+++ model2.php
@@ -0,0 +1,30 @@
+<?php 
+class Model
+{
+    protected static $table = null;
+
+    public static function getTableName()
+    {
+        return static::$table;
+    }
+    // Array to store our key/value data
+    protected $attributes = [];
+
+    // Magic setter to populate $data array
+    public function __set($nameOfProperty, $value)
+    {
+        // Set the $name key to hold $value in $data
+        $this->attributes[$nameOfProperty] = $value;
+    }
+
+    // Magic getter to retrieve values from $data
+    public function __get($nameOfPropertyWeAreTryingToGet)
+    {
+        // Check for existence of array key $name
+        if (array_key_exists($nameOfPropertyWeAreTryingToGet, $this->attributes)) {
+            return $this->attributes[$nameOfPropertyWeAreTryingToGet];
+        }
+        return null;
+    }
+
+}
